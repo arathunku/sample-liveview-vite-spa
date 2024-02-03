60d518971641cbc30788140d872927bc7ec3d4c2
arathunku <m@forys.de>
2024-02-03 12:07:18 +0100

    Add links in SPA to LiveView

--- assets/sample/src/App.jsx
+++ assets/sample/src/App.jsx
@@ -6,11 +6,42 @@ import {
   Link,
   Outlet,
 } from "react-router-dom";
+import ReactSvg from "./assets/react.svg";
+
+// see: https://github.com/phoenixframework/phoenix_live_view/blob/d41ea3b55b2a6a74c3a5634737f0a998042b32f3/lib/phoenix_component.ex#L2667
+const LiveLink = ({ navigate, href, replace, ...props }) => {
+  const dataProps = {}
+
+  if (navigate) {
+    dataProps["data-phx-link"] = "redirect"
+    dataProps["data-phx-link-state"] = replace ? "replace" : "push"
+  }
+
+  return <Link to={navigate || href || "#"} {...props} {...dataProps} />;
+};
 
 const Root = (props) => (
-  <div>
-    <Outlet />
-  </div>
+  <>
+    <header className="px-4 sm:px-6 lg:px-8">
+      <div className="flex items-center justify-between border-b border-zinc-100 py-3 text-sm">
+        <div className="flex items-center gap-4">
+          <Link to="/">
+            <img src={ReactSvg} alt="React SVG" className="h-8 w-8" />
+          </Link>
+        </div>
+        <div className="flex items-center gap-4 font-semibold leading-6 text-zinc-900">
+          <Link to="about">About</Link>
+          <Link to="contact">Contact</Link>
+          <LiveLink navigate="login">Login</LiveLink>
+        </div>
+      </div>
+    </header>
+    <main className="px-4 py-20 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-2xl">
+        <Outlet />
+      </div>
+    </main>
+  </>
 );
 
 // Taken mostly from https://reactrouter.com/en/main/start/overview
@@ -20,17 +51,10 @@ const router = createBrowserRouter(
       <Route
         path=""
         element={
-          <ul>
-            <li>
-              <Link to="about">About Us</Link>
-            </li>
-            <li>
-              <Link to="contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="login">Login</Link>
-            </li>
-          </ul>
+          <>
+            <p>Landing page</p>
+            <p>Cheers from React</p>
+          </>
         }
       />
       <Route
@@ -38,7 +62,6 @@ const router = createBrowserRouter(
         element={
           <>
             <div>About</div>
-            <Link to="/">Go back</Link>
           </>
         }
       />
@@ -47,7 +70,6 @@ const router = createBrowserRouter(
         element={
           <>
             <div>Contact</div>
-            <Link to="/">Go back</Link>
           </>
         }
       />
@@ -56,7 +78,6 @@ const router = createBrowserRouter(
         element={
           <>
             <div>Login</div>
-            <Link to="/">Go back</Link>
           </>
         }
       />
